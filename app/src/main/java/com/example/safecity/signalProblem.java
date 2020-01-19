335e63aa313928c064372721d6189841f5009113
aicha <miloudi.ai04@gmail.com>
2020-01-18 20:21:05 -0500

    added take picture feature

--- app/src/main/java/com/example/safecity/signalProblem.java
+++ app/src/main/java/com/example/safecity/signalProblem.java
@@ -2,15 +2,23 @@ package com.example.safecity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 public class signalProblem extends AppCompatActivity {
     private Spinner spinner;
+    public ImageView imageView;
+    public ImageButton takePictureButton;
     private static final String[] items = {"item 1", "item 2", "item 3"};
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +29,31 @@ public class signalProblem extends AppCompatActivity {
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
-
+        takePictureButton = (ImageButton) findViewById(R.id.camera_button);
+        imageView = (ImageView) findViewById(R.id.imageView);
     }
     public void signal(View view){
 
     }
     public void takePicture(View view){
-
+        this.dispatchTakePictureIntent();
     }
     public void browsePicture(View view){
 
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 }
